b68de75dfc3375b91ab1d4131e2e73e12bec4ccc
Natalya <gruzdevanatalya5@gmail.com>
2022-08-05 05:56:50 +0300

    adaptive part1

--- js/main.js
+++ js/main.js
@@ -4,29 +4,32 @@ const owl = $('.owl-carousel');
 owl.owlCarousel({
 	center: false,
 	loop: true,
-	margin: 90,
+	margin: 163,
 	startPosition: 0,
 	items: 1,
 	responsive: {
 		1920: {
 			items: 4,
-			startPosition: 1,
+			startPosition: 0,
 		},
 		1366: {
 			items: 3,
-			startPosition: 1,
+			startPosition: 0,
+			margin: 146,
 		},
 		1024: {
 			items: 3,
-			startPosition: 1,
+			startPosition: 0,
+			margin: 70,
 		},
 		768: {
 			items: 3,
-			startPosition: 1,
+			startPosition: 0,
+			margin: 61,
 		},
 		375: {
 			items: 1,
-			startPosition: 1,
+			startPosition: 0,
 		},
 
 	},
@@ -39,4 +42,12 @@ $('.slider__btn--prev').click(function () {
 
 $('.slider__btn--next').click(function () {
 	owl.trigger('next.owl.carousel');
+});
+
+
+$(document).ready(function() {
+    $('.header__burger').click(function(event){
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
 });
